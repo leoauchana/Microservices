4a3905e4784fe9b7aa75835c39acbfeabaa6b111
Leoauchana <matiasleonelcja@gmail.com>
2026-04-23 18:04:19 -0300

    add: Se agrego la propiedad Description a la entidad producto

--- Microservices/ProductService/Src/Domain/Entities/Product.cs
+++ Microservices/ProductService/Src/Domain/Entities/Product.cs
@@ -4,13 +4,15 @@ public class Product
 {
     public Guid Id { get; private set; }
     public string Name { get; private set; } = null!;
+    public string Description { get; private set; } = null!;
     public float Price { get; private set; }
     public int Stock { get; private set; }
 
-    public Product(string name, float price, int stock)
+    public Product(string name, string description, float price, int stock)
     {
         Id = Guid.NewGuid();
         Name = name;
+        Description = description;
         Price = price;
         Stock = stock;
     }
