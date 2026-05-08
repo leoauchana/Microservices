409f16e99f42d7bd272a698941fc9b30ecac895c
Leoauchana <matiasleonelcja@gmail.com>
2026-05-08 13:11:35 -0300

    fix:: Se quito la capa transversal y se centralizo todos los servicios no relacionados con el negocio en la capa de infraestructura

---
+++ Microservices/ProductService/Src/Infraestructure/Configurations/DatabaseOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Infraestructure.Configurations;
+
+public class DatabaseOptions
+{
+    public const string Section = "ConnectionStrings";
+
+    public string Db_Product { get; set; } = string.Empty;
+
+}
---
+++ Microservices/ProductService/Src/Infraestructure/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces;
+using Infraestructure.Configurations;
+using Infraestructure.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Infraestructure;
+
+public static class ServiceExtensions
+{
+    public static void AddInfraestructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<DatabaseOptions>()
+            .Bind(configuration.GetSection(DatabaseOptions.Section))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddDbContext<Data.Context.ProductServiceContext>((opt, conf) =>
+        {
+            var dbOptions = opt.GetRequiredService<IOptions<DatabaseOptions>>().Value;
+            conf.UseNpgsql(dbOptions.Db_Product);
+        });
+        services.AddScoped<IRepository, Repository>();
+    }
+}
